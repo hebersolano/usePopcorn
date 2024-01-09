71f69ebe0979bf81ce1468c5aca209402d6a0b01
Heber Solano <hebersolano1@gmail.com>
2024-01-09 13:03:32 -0600

    star rating component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+// import "./index.css";
+import StarRating from "./StarRating.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+function Test() {
+  const [ratingMovie, setRatingMovie] = useState(0);
+  return (
+    <>
+      <StarRating maxRating={10} color="blue" onSetRating={setRatingMovie} />
+      <p>This is the rating {ratingMovie}</p>
+    </>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <StarRating maxRating={5} defaultRating={3} />
+    <StarRating maxRating={5} messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]} />
+    <Test />
+  </React.StrictMode>
+);
